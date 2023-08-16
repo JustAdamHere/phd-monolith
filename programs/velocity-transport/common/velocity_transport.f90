9eaef1af967e9813eefc4389cc96aa67a3c5b4df
Adam Blakey <adam@blakey.family>
2023-08-16 17:07:47 +0100

    Fixed for single Re step.

--- programs/velocity-transport/common/velocity_transport.f90
+++ programs/velocity-transport/common/velocity_transport.f90
@@ -249,23 +249,30 @@ program velocity_transport
         !         jacobian_face_velocity_ss, 1)
         !     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         ! end if
-    
-        ! Creates "base steps" of size 0.1 through to 0.01 between different Reynolds numbers.
-        max_step = -1
-        min_step = -2
-        allocate(steps(no_reynold_ramp_steps))
-        do i = 1, no_reynold_ramp_steps - 1
-            steps(no_reynold_ramp_steps - i + 1) = 10**(min_step + real(i-1)*real(max_step - min_step)/(no_reynold_ramp_steps-2))
-        end do
-        steps(1) = 0.0_db
         
-        ! "Normalise" the steps.
-        steps = steps / sum(steps)
-        steps = steps * (1 - reynold_ramp_start_ratio)
+        if (no_reynold_ramp_steps == 1) then
+            allocate(steps(1))
+            steps(1) = 0.0_db
+            ratio    = 1.0_db
+        else
+            ! Creates "base steps" of size 0.1 through to 0.01 between different Reynolds numbers.
+            max_step = -1
+            min_step = -2
+            allocate(steps(no_reynold_ramp_steps))
+            do i = 1, no_reynold_ramp_steps - 1
+                steps(no_reynold_ramp_steps - i + 1) = &
+                    10**(min_step + real(i-1)*real(max_step - min_step)/(no_reynold_ramp_steps-2))
+            end do
+            steps(1) = 0.0_db
 
+            ! "Normalise" the steps.
+            steps = steps / sum(steps)
+            steps = steps * (1 - reynold_ramp_start_ratio)
+
+            ratio                                = reynold_ramp_start_ratio
+        end if
         velocity_convection_coefficient_orig = velocity_convection_coefficient
         velocity_time_coefficient_orig       = velocity_time_coefficient
-        ratio                                = reynold_ramp_start_ratio
         do reynold_step = 1, no_reynold_ramp_steps
             ratio = ratio + steps(reynold_step)
 
