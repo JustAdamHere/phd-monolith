b0f36701ea19151c9bd432f5e212a65fde7ab593
Adam Blakey <adam@blakey.family>
2023-10-26 14:01:20 +0100

    * Select no_threads from file.

--- miscellaneous/select_no_threads.py
+++ miscellaneous/select_no_threads.py
@@ -6,3 +6,17 @@ def select_no_threads(prev_threads, load_avg, max_threads, min_threads):
   no_threads = math.floor(b)
 
   return no_threads
+
+def read_no_threads(default_no_threads):
+  try:
+    file = open(f"./output/monolith_no_threads.txt", "r")
+  except:
+    file = open(f"./output/monolith_no_threads.txt", "w")
+    file.write(f"{default_no_threads}")
+    file.close()
+    file = open(f"./output/monolith_no_threads.txt", "r")
+    
+  no_threads = int(file.read())
+  file.close()
+
+  return int(no_threads)
