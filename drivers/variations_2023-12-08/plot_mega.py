6398ca3f4bdce6896c489248f00df3ae685e3d85
Adam Blakey <adam@blakey.family>
2024-02-15 18:52:42 +0000

    NEEDS TESTING: added ratio to vessel mega plot.

--- drivers/variations_2023-12-08/plot_mega.py
+++ drivers/variations_2023-12-08/plot_mega.py
@@ -126,19 +126,25 @@ def plot_vessels(simulations, simulation_bins, parameter_values, parameter_name,
   print(f"\rPlotting simulations...", end="")
   no_bins = len(simulation_bins[0][0])
 
-  assert(len(parameter_values) == 2)
-  assert(len(simulation_bins) == 2)
+  assert(len(parameter_values) == 3)
+  assert(len(simulation_bins) == 3)
   assert(len(simulation_bins[0]) == 2)
+  assert(len(simulation_bins[1]) == 2)
+  assert(len(simulation_bins[2]) == 1)
 
   # Setup plots.
   from plotting import setup_plots
-  fig, axes = setup_plots.setup_megaplot(1, 8, 2, figsize=(8, 18))
+  fig, axes = setup_plots.setup_megaplot(1, 8, 3, figsize=(8, 18))
 
   # Get data to plot.
-  data = np.zeros((2, 2), dtype=object)
-  for j in range(2):
+  data = np.zeros((2, 3), dtype=object)
+  for j in range(3):
     for i in range(2):
-      data[i, j] = setup_plots.get_data(len(simulation_bins[j][i]), simulation_bins[j][i], simulations)
+      # TODO: Real nasty hack to make this run. To fix.
+      if (j == 2 and i == 1):
+        data[1, 2] = setup_plots.get_data(len(simulation_bins[2][0]), simulation_bins[2][0], simulations)
+      else:
+        data[i, j] = setup_plots.get_data(len(simulation_bins[j][i]), simulation_bins[j][i], simulations)
 
   # Patches for legends.
   import matplotlib.patches as mpatches
@@ -148,7 +154,7 @@ def plot_vessels(simulations, simulation_bins, parameter_values, parameter_name,
     handles.append(mpatches.Patch(color=f"C{i}"))
 
   # AXES 1: Velocity magnitude integrals.
-  for j in range(2):
+  for j in range(3):
     for i in range(2):
       axes[0][j].plot(parameter_values[j], data[i, j]["q50"]["velocity_magnitude_integral"], linestyle="dashed", color=f"C{i}")
       axes[0][j].fill_between(parameter_values[j], data[i, j]["q25"]["velocity_magnitude_integral"], data[i, j]["q75"]["velocity_magnitude_integral"], alpha=0.2, color=f"C{i}")
@@ -157,7 +163,7 @@ def plot_vessels(simulations, simulation_bins, parameter_values, parameter_name,
   axes[0][1].legend(handles=handles[0:2], labels=["any arteries", "6 arteries"])
 
   # AXES 2: Slow velocity percentages.
-  for j in range(2):
+  for j in range(3):
     axes[1][j].plot(parameter_values[j], data[0, j]["q50"]["slow_velocity_percentage_ivs"], linestyle="dashed", color="C0")
     axes[1][j].plot(parameter_values[j], data[0, j]["q50"]["slow_velocity_percentage_everywhere"], linestyle="dashed", color="C1")
     axes[1][j].plot(parameter_values[j], data[0, j]["q50"]["slow_velocity_percentage_dellschaft"], linestyle="dashed", color="C2")
@@ -169,7 +175,7 @@ def plot_vessels(simulations, simulation_bins, parameter_values, parameter_name,
     axes[1][j].legend(handles=handles[0:4], labels=["IVS", "everywhere", "Dellschaft", "nominal everywhere"])
 
   # AXES 3: Velocity flux through different veins.
-  for j in range(2):
+  for j in range(3):
     axes[2][j].plot(parameter_values[j], data[0, j]["q50"]["velocity_percentage_basal_plate"], linestyle="dashed", color="C0")
     axes[2][j].plot(parameter_values[j], data[0, j]["q50"]["velocity_percentage_septal_wall"], linestyle="dashed", color="C1")
     axes[2][j].plot(parameter_values[j], data[0, j]["q50"]["velocity_percentage_marginal_sinus"], linestyle="dashed", color="C2")
@@ -179,7 +185,7 @@ def plot_vessels(simulations, simulation_bins, parameter_values, parameter_name,
     axes[2][j].legend(handles=handles[0:3], labels=["basal plate", "septal wall", "marginal sinus"])
 
   # AXES 4: Cross-flux velocity.
-  for j in range(2):
+  for j in range(3):
     for i in range(2):
       axes[3][j].plot(parameter_values[j], data[i, j]["q50"]["velocity_cross_flow_flux"], linestyle="dashed", color=f"C{i}")
       axes[3][j].fill_between(parameter_values[j], data[i, j]["q25"]["velocity_cross_flow_flux"], data[i, j]["q75"]["velocity_cross_flow_flux"], alpha=0.2, color=f"C{i}")
@@ -187,7 +193,7 @@ def plot_vessels(simulations, simulation_bins, parameter_values, parameter_name,
   axes[3][1].legend(handles=handles[0:2], labels=["any arteries", "6 arteries"])
 
   # AXES 5: Transport reaction integral.
-  for j in range(2):
+  for j in range(3):
     for i in range(2):
       axes[4][j].plot(parameter_values[j], data[i, j]["q50"]["transport_reaction_integral"], linestyle="dashed", color=f"C{i}")
       axes[4][j].fill_between(parameter_values[j], data[i, j]["q25"]["transport_reaction_integral"], data[i, j]["q75"]["transport_reaction_integral"], alpha=0.2, color=f"C{i}")
@@ -195,7 +201,7 @@ def plot_vessels(simulations, simulation_bins, parameter_values, parameter_name,
   axes[4][1].legend(handles=handles[0:2], labels=["any arteries", "6 arteries"])
 
   # AXES 6: Concentration flux through different veins.
-  for j in range(2):
+  for j in range(3):
     axes[5][j].plot(parameter_values[j], data[0, j]["q50"]["transport_percentage_basal_plate"], linestyle="dashed", color="C0")
     axes[5][j].plot(parameter_values[j], data[0, j]["q50"]["transport_percentage_septal_wall"], linestyle="dashed", color="C1")
     axes[5][j].plot(parameter_values[j], data[0, j]["q50"]["transport_percentage_marginal_sinus"], linestyle="dashed", color="C2")
@@ -205,7 +211,7 @@ def plot_vessels(simulations, simulation_bins, parameter_values, parameter_name,
     axes[5][j].legend(handles=handles[0:3], labels=["basal plate", "septal wall", "marginal sinus"])
 
   # AXES 7: Kinetic energy flux difference.
-  for j in range(2):
+  for j in range(3):
     for i in range(2):
       axes[6][j].plot(parameter_values[j], data[i, j]["q50"]["kinetic_energy_flux"], linestyle="dashed", color=f"C{i}")
       axes[6][j].fill_between(parameter_values[j], data[i, j]["q25"]["kinetic_energy_flux"], data[i, j]["q75"]["kinetic_energy_flux"], alpha=0.2, color=f"C{i}")
@@ -213,7 +219,7 @@ def plot_vessels(simulations, simulation_bins, parameter_values, parameter_name,
   axes[6][1].legend(handles=handles[0:2], labels=["any arteries", "6 arteries"])
 
   # AXES 8: Total energy flux difference.
-  for j in range(2):
+  for j in range(3):
     for i in range(2):
       axes[7][j].plot(parameter_values[j], data[i, j]["q50"]["total_energy_flux"], linestyle="dashed", color=f"C{i}")
       axes[7][j].fill_between(parameter_values[j], data[i, j]["q25"]["total_energy_flux"], data[i, j]["q75"]["total_energy_flux"], alpha=0.2, color=f"C{i}")
@@ -238,6 +244,15 @@ def plot_vessels(simulations, simulation_bins, parameter_values, parameter_name,
   setup_plots.style(fig, axes[5][1], None, None, y_scilimits=None , y_top=102, integer_ticks=True)
   setup_plots.style(fig, axes[6][1], None, None, y_scilimits=[-3, -3], y_bottom=0, y_top=2e-3, integer_ticks=True)
   setup_plots.style(fig, axes[7][1], parameter_name[1], None, y_scilimits=[4, 4], y_bottom=0, y_top=1e5, integer_ticks=True)
+
+  setup_plots.style(fig, axes[0][2], None, None, y_scilimits=[-3, -3], y_bottom=0, y_top=1e-2, integer_ticks=True)
+  setup_plots.style(fig, axes[1][2], None, None, y_scilimits=None , y_top=102, integer_ticks=True)
+  setup_plots.style(fig, axes[2][2], None, None, y_scilimits=None , y_top=102, integer_ticks=True)
+  setup_plots.style(fig, axes[3][2], None, None, y_scilimits=[-3, -3], y_bottom=0, y_top=2e-2, integer_ticks=True)
+  setup_plots.style(fig, axes[4][2], None, None, y_scilimits=[-3, -3], y_bottom=0, y_top=2e-3, integer_ticks=True)
+  setup_plots.style(fig, axes[5][2], None, None, y_scilimits=None , y_top=102, integer_ticks=True)
+  setup_plots.style(fig, axes[6][2], None, None, y_scilimits=[-3, -3], y_bottom=0, y_top=2e-3, integer_ticks=True)
+  setup_plots.style(fig, axes[7][2], parameter_name[2], None, y_scilimits=[4, 4], y_bottom=0, y_top=1e5, integer_ticks=True)
 
   # Decide where to save plots.
   if subfolder == None:
