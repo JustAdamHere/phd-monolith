e125cda7a7a23ea89cb49dd840e53ba585183e55
Adam Blakey <adam@blakey.family>
2024-02-20 08:19:07 +0000

    Removed ratio plots.

--- drivers/variations_2023-12-08/vary_mega-vessels.py
+++ drivers/variations_2023-12-08/vary_mega-vessels.py
@@ -8,12 +8,11 @@ images_location = None
 simulations = run_data.import_simulations(1400, output_location)
   
 # Varying parameters.
-parameter_name      = ["number of arteries", "number of veins", "veins to arteries ratio"]
-parameter_safe_name = ["no-arteries", "no-veins", "veins-to-arteries"]
+parameter_name      = ["number of arteries", "number of veins"]
+parameter_safe_name = ["no-arteries", "no-veins"]
 artery_values       = np.linspace(1, 6,  6)
 vein_values         = np.linspace(0, 27, 28)
-ratio_values        = np.linspace(0, 6, 7)
-parameter_values    = [artery_values, vein_values, ratio_values]
+parameter_values    = [artery_values, vein_values]
 
 
 bins_no_arteries_all_veins = [[] for i in range(len(artery_values))]
@@ -56,30 +55,25 @@ for i in range(0, 1400):
   if (no_arteries == 6) and (i < 1000 or (1200 <= i and i < 1300)):
     bins_no_veins_6_arteries[no_veins].append(run_no)
 
-bins_veins_to_arteries = [[] for i in range(len(ratio_values))]
-for i in range(0, 1000):
-  run_no = i+1
+# bins_veins_to_arteries = [[] for i in range(len(ratio_values))]
+# for i in range(0, 1000):
+#   run_no = i+1
 
-  no_veins    = simulations[i].get_no_veins()
-  no_arteries = simulations[i].get_no_arteries()
+#   no_veins    = simulations[i].get_no_veins()
+#   no_arteries = simulations[i].get_no_arteries()
 
-  if (no_veins == 0):
-    continue
+#   if (no_veins == 0):
+#     continue
 
-  ratio = float(no_arteries)/float(no_veins)
+#   ratio = float(no_arteries)/float(no_veins)
 
-  bin_no = int(np.floor((len(ratio_values)-1)*(ratio - ratio_values[0])/(ratio_values[-1] - ratio_values[0])))
-  if (bin_no == len(ratio_values)):
-    bin_no -= 1
-  bins_veins_to_arteries[bin_no].append(run_no)
+#   bin_no = int(np.floor((len(ratio_values)-1)*(ratio - ratio_values[0])/(ratio_values[-1] - ratio_values[0])))
+#   if (bin_no == len(ratio_values)):
+#     bin_no -= 1
+#   bins_veins_to_arteries[bin_no].append(run_no)
 
 # Plot the data.
 import plot_mega
-bins = [[bins_no_arteries_all_veins, bins_no_arteries_27_veins], [bins_no_veins_all_arteries, bins_no_veins_6_arteries], [bins_veins_to_arteries]]
-
-# print(f"Zero veins simulations: {bins[1][0][0]}")
-# print(f"1 vein simulations: {bins[1][0][1]}")
-# print(f"10 veins simulations: {bins[1][0][10]}")
-# print(f"27 veins simulations: {bins[1][0][27]}")
+bins = [[bins_no_arteries_all_veins, bins_no_arteries_27_veins], [bins_no_veins_all_arteries, bins_no_veins_6_arteries]]
 
 plot_mega.plot_vessels(simulations, bins, parameter_values, parameter_name, parameter_safe_name, images_location)
