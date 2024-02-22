ee9347686736f4b87b643eee9175330091b8fc2b
Adam Blakey <adam@blakey.family>
2024-02-22 09:40:43 +0000

    Changed matfile to v7.3.

--- mri_code/main.m
+++ mri_code/main.m
@@ -41,6 +41,6 @@ parfor voxel = 1:N_voxels
   end
 end
 
-save(strcat('../output/mri-quantities_', filename_no_ext, '_', num2str(run_no), '.mat'))
+save(strcat('../output/mri-quantities_', filename_no_ext, '.mat'), "-v7.3")
 
 fprintf("Got quantities.\n")
