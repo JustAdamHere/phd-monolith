a3aa3aef2853775b5e0bb6a41426b7661c6d1a90
Adam Blakey <adam@blakey.family>
2024-03-01 10:39:29 +0000

    +

--- miscellaneous/output.py
+++ miscellaneous/output.py
@@ -23,3 +23,27 @@ def end_execution(sig, frame):
   output.output("⚠️ Termination signal received! Saving output so far...", True)
   output.save()
   exit(0)
+
+def display_run_output(run_process, output_filename, terminal_output, verbose_output, end_message=""):
+	# Display last line of output to screen, and write lines to file.
+	line_truncation = 123
+	if (verbose_output):
+		end = '\r\n'
+	else:
+		end = '\r'
+	run_output = open(f"./output/{output_filename}.txt", "w")
+	output("", terminal_output)
+	while run_process.poll() is None:
+		line = run_process.stdout.readline().decode('utf-8').rstrip('\r\n')
+		if (line != ""):
+			output(f">>> {line[:line_truncation]:<{line_truncation}}", terminal_output, end='')
+			if (len(line) > line_truncation):
+				output("...", terminal_output, end=end)
+			else:
+				output("", terminal_output, end=end)
+			run_output.write(line + '\n')
+	run_output.close()
+	if (verbose_output):
+		output("", terminal_output, end=f'\r{end_message} ')
+	else:
+		output("", terminal_output, end=f'\x1b[1A\r{end_message} ')
