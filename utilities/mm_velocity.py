bf004ef91acc6a989ee4ff26efd19712acd7d39d
Adam Blakey <adam@blakey.family>
2024-03-25 10:09:27 +0000

    + Constant moving mesh velocity. ~ Modified the Python mm velocity file to correctly calculate the mesh velocity to get desired change in area.

--- utilities/mm_velocity.py
+++ utilities/mm_velocity.py
@@ -1,74 +1,29 @@
 import numpy as np
 
-# SOMETHING INCORRECT HERE, TO DO WITH VOLUME CONSISTING OF CAP AND DISK
-# # Placenta dimensions.
-# placenta_width  = 22 # 22cm
-# placenta_height = 3.626 # 3.626cm
-# ms_pipe_width_max   = 0.3   # cm
-# dome_centre_x = placenta_width/2
-# dome_centre_y = (placenta_height - ms_pipe_width_max)/2 + dome_centre_x**2/(2*(placenta_height - ms_pipe_width_max))
-# placenta_dome_radius = dome_centre_y
+def get_segment_area(h, r):
+  # Area of segment [https://www.omnicalculator.com/math/segment-area].
+  return r**2*np.arccos((r-h)/r) - (r-h)*np.sqrt(2*r*h-h**2)
 
-# print(f"Placenta dimensions: width={placenta_width}, height={placenta_height}, ms_pipe_width_max={ms_pipe_width_max}, dome_centre_x={dome_centre_x}, dome_centre_y={dome_centre_y}, placenta_dome_radius={placenta_dome_radius}")
-
-# # Initial geometry dimensions with 3D volume.
-# h_max = placenta_height - ms_pipe_width_max # Cap height.
-# a_max = 11.0  # Cap base radius.
-# r_max = placenta_dome_radius
-# cap_volume = (1/6)*np.pi*h_max*(3*a_max**2 + h_max**2) # Volume of cap [https://www.omnicalculator.com/math/sphere-volume].
-# disk_volume = np.pi*r_max**2*ms_pipe_width_max # Volume of disk.
-# V_max = cap_volume + disk_volume
-
-# print(f"Initial 3D dimensions: h_max={h_max}, a_max={a_max}, r_max={r_max}, cap_volume={cap_volume}, disk_volume={disk_volume}")
-# print(f"Initial 3D volume: V_max={V_max}")
-
-# # Initial 2D area.
-# chord_area = r_max**2*np.arccos((r_max-h_max)/r_max) - (r_max-h_max)*np.sqrt(2*r_max*h_max-h_max**2)
-# rectangle_area = a_max*ms_pipe_width_max
-# A_max = chord_area + rectangle_area
-
-# print(f"Initial 2D area: A_max={A_max}")
-
-# # 3D volume reduction.
-# volume_reduction = 0.35
-# V_min = V_max*(1-volume_reduction)
-
-# print(f"3D volume reduction by {100*volume_reduction}%: V_min={V_min}")
-
-# # Corresponding cap height and radius, assuming they remain proportional.
-# k1 = a_max/h_max
-# k2 = ms_pipe_width_max/h_max
-# k3 = r_max/h_max
-# h_min = (6*V_min/(np.pi*(3*k1**2+1)))**(1/3)
-# a_min = k1*h_min
-# ms_pipe_width_min = k2*h_min
-# r_min = k3*h_min
-
-# print(f"Deflated 3D dimensions: h_min={h_min}, a_min={a_min}, ms_pipe_width_min={ms_pipe_width_min}, r_min={r_min}")
-
-# # Corresponding 2D area.
-# chord_area = r_min**2*np.arccos((r_min-h_min)/r_min) - (r_min-h_min)*np.sqrt(2*r_min*h_min-h_min**2)
-# rectangle_area = a_min*ms_pipe_width_min
-# A_min = chord_area + rectangle_area
-
-# print(f"Equivalent 2D volume, reduced by {100*(1-A_min/A_max)}%: A_min={A_min}")
+def get_spherical_cap_volume(h, a):
+  # Volume of cap [https://www.omnicalculator.com/math/sphere-volume].
+  return (1/6)*np.pi*h*(3*(a)**2 + h**2)
 
 # Placenta dimensions.
-placenta_width  = 22 # 22cm
-placenta_height = 3.626 # 3.626cm
-ms_pipe_width_max   = 0.3   # cm
+placenta_width  = 22e-2/0.04 # 22cm
+placenta_height = 3.626e-2/0.04 # 3.626cm
+ms_pipe_width_max   = 0.3e-2/0.04 # 0.3cm
 dome_centre_x = placenta_width/2
 dome_centre_y = (placenta_height - ms_pipe_width_max)/2 + dome_centre_x**2/(2*(placenta_height - ms_pipe_width_max))
 placenta_dome_radius = dome_centre_y
 
-print(f"Placenta dimensions: width={placenta_width}, height={placenta_height}, ms_pipe_width_max={ms_pipe_width_max}, dome_centre_x={dome_centre_x}, dome_centre_y={dome_centre_y}, placenta_dome_radius={placenta_dome_radius}")
+print(f"Placenta dimensions: width={placenta_width:.2f}, height={placenta_height:.2f}, ms_pipe_width_max={ms_pipe_width_max:.2f}, dome_centre_x={dome_centre_x:.2f}, dome_centre_y={dome_centre_y:.2f}, placenta_dome_radius={placenta_dome_radius:.2f}")
 
 # Initial geometry dimensions with 3D volume.
 h_max = placenta_height # Cap height.
-a_max = 11.0  # Cap base radius.
-a_hat_max = 0.437 # Extra radius for curve.
+a_max = 11.0e-2/0.04  # Cap base radius.
+a_hat_max = 0.437e-2/0.04 # Extra length for curve.
 r_max = placenta_dome_radius
-cap_volume = (1/6)*np.pi*h_max*(3*(a_max+a_hat_max)**2 + h_max**2) # Volume of cap [https://www.omnicalculator.com/math/sphere-volume].
+cap_volume = get_spherical_cap_volume(h_max, a_max + a_hat_max)
 V_max = cap_volume
 
 print(f"Initial 3D dimensions: h_max={h_max}, a_max+a_hat_max={a_max+a_hat_max}, r_max={r_max}")
@@ -82,9 +37,12 @@ print(f"Initial 2D area: A_max={A_max}")
 
 # 3D volume reduction.
 volume_reduction = 0.35
+area_reduction = 1 - (1-volume_reduction)**(2/3)
 V_min = V_max*(1-volume_reduction)
+A_min = A_max*(1-area_reduction)
 
 print(f"3D volume reduction by {100*volume_reduction}%: V_min={V_min}")
+print(f"2D area reduction by {100*area_reduction:.2f}%: A_min={A_min}")
 
 # Corresponding cap height and radius, assuming they remain proportional.
 k1 = a_max/h_max
@@ -98,20 +56,12 @@ r_min = k3*h_min
 a_hat_min = k4*h_min
 
 print(f"Deflated 3D dimensions: h_min={h_min}, a_min+a_hat_min={a_min+a_hat_min}, ms_pipe_width_min={ms_pipe_width_min}, r_min={r_min}")
+print(f"Deflated 3D volume: V_min={get_spherical_cap_volume(h_min, a_min+a_hat_min)}")
+print(f"Deflated 2D area: A_min={get_segment_area(h_min, r_min)}")
 
-# Corresponding 2D area.
-chord_area = r_min**2*np.arccos((r_min-h_min)/r_min) - (r_min-h_min)*np.sqrt(2*r_min*h_min-h_min**2)
-A_min = chord_area
+p = [placenta_width/2, placenta_height]
 
-print(f"Equivalent 2D volume, reduced by {100*(1-A_min/A_max):.2f}%: A_min={A_min:.2f}")
-
-# Top left of placenta, not including marginal sinus vein.
-p = [placenta_width, placenta_height]
-
-# Maximal displacement of p required to give desired area change.
-s_target = a_max - a_min
-
-print(f"Target displacement: {a_max:.2f}-{a_min:.2f}={s_target:.2f}")
+print(f"Initial position: {p}")
 
 ## Calculate the best W to approximate the target_s ##
 # Centre about the inflation.
@@ -119,57 +69,66 @@ x_c = np.array([placenta_width/2, placenta_height/2])
 
 # Oscillating domain velocity.
 def w(W, x, x_c, t):
-  return W*np.sin(2*np.pi*t)*(x - x_c)
+  #return W*np.sin(2*np.pi*t)*(x - x_c)
+  return np.where(t<0.5, W*(x - x_c), -W*(x - x_c))
 
 # Displacement at t=0.5 and t=1.
-def displacement(W, p0, x_c, t):
+def p_positions(W, p0, x_c, t):
   p = p0
   N = len(t)
-  for i in range(len(t)):
+  for i in range(int(N/2)):
     p += dt*w(W, p, x_c, t[i])
-    if (i == N/2 or i+1 == N/2):
-      s_mid = p[0]
-  s_end = p[0]
+  x_mid = p.copy()
+  for i in range(int(N/2), N):
+    p += dt*w(W, p, x_c, t[i])
+  x_end = p
 
-  return [s_mid, s_end]
+  return [x_mid, x_end]
 
 # Problem parameters.
 T = 60*16*2/0.1143 # 2*16 minutes, divided by scaling
-N = 1000
+N = 1000#100000
 dt = T/N
 t = np.linspace(0, T, N)
 
 print(f"Time step: {dt}, total time: {T}, number of time steps: {N}")
 
 # Bounds of W.
-W_max = -0.00001
-W_min = -0.01
-s_max = p[0] - displacement(W_max, p, x_c, t/T)[0]
-s_min = p[0] - displacement(W_min, p, x_c, t/T)[0]
+W_max = -1e-10
+W_min = -1e-3
 
-print(f"Initial domain velocity bounds: [{W_min}, {W_max}]")
-print(f"Initial displacement bounds: [{s_min}, {s_max}]")
-print(f"Initial position: {p}")
+print(f"Initial velocity bounds: [{W_min}, {W_max}]")
 
-s = 1000
-N_max = 10
-N = 0
-while (np.abs(s - s_target) >= 1e-5) and (N < N_max):
-  N += 1
-  assert((s_max-s_target)*(s_min-s_target) < 0)
-  W = (W_max + W_min)/2
-  s, s_end = displacement(W, p, x_c, t/T)
-  if ((s-s_target)*(s_max-s_target) < 0):
+def get_reduced_area(W, p, x_c, t, r_max, h_max):
+  x = p_positions(W, p, x_c, t/T)[0]
+  s = p - x
+  h = x[1] - s[1]
+  k5 = r_max/h_max
+  r = h*k5
+  A = get_segment_area(h, r)
+
+  return A
+
+# Do bisection.
+A_test_min = get_reduced_area(W_min, p, x_c, t, r_max, h_max)
+A_test_max = get_reduced_area(W_max, p, x_c, t, r_max, h_max)
+
+print(f"Initial reduced area bounds: [{A_test_min}, {A_test_max}]")
+
+W = (W_min + W_max)/2
+A = get_reduced_area(W, p, x_c, t, r_max, h_max)
+N_itns = 0
+while ((np.abs(A_min - A) > 1e-5) and (N_itns < 100)):
+  N_itns += 1
+  W = (W_min + W_max)/2
+  A = get_reduced_area(W, p, x_c, t, r_max, h_max)
+  if (A < A_min):
     W_min = W
-    s_min = s
+    A_test_min = A
   else:
     W_max = W
-    s_max = s
+    A_test_max = A
+  print(f"Iteration {N_itns}: W={W}, A={A}")
 
-  print(f"W_min={W_min}, W_max={W_max}, s={s}, s_diff={s-s_target}, s_end={s_end}")
-
-print(f"W={W}, s_end={p[0]-s_end}")
-
-# N = 10000: W=-2.685757637023926e-05
-# N = 1000: W=-2.6877875328063972e-05
-# N = 100: W=-2.7107772827148444e-05
+print(f"Complete in {N_itns} iterations")
+print(f"Final domain velocity: {W}, area: {A}, target area: {A_min}, diff = {A - A_min}")
