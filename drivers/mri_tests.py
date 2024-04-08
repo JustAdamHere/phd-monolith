~ Swapped order of MRI tests, and changed sign on accelerating test. ! Fixed bug with plotting M's magnitude on spin graphs. ~ Other misc changes to presentation.

drivers/mri_tests.py
@@ -38,13 +38,13 @@ parameters["mri_u2"  ] = 0.01
 velocity_transport.run(1, parameters)
 
 parameters["geometry"] = 'shear_test'
-parameters["mri_u1"  ] = 0.01
-parameters["mri_u2"  ] = 0.01
+parameters["mri_u1"  ] =  0.0025
+parameters["mri_u2"  ] = -0.0025
 velocity_transport.run(2, parameters)
 
 parameters["geometry"] = 'shear_test'
-parameters["mri_u1"  ] =  0.0025
-parameters["mri_u2"  ] = -0.0025
+parameters["mri_u1"  ] = 0.01
+parameters["mri_u2"  ] = 0.01
 velocity_transport.run(3, parameters)
 
 parameters["geometry"] = 'rotational_test'
@@ -54,7 +54,7 @@ velocity_transport.run(4, parameters)
 
 import numpy as np
 parameters["geometry"] = 'accelerating_test'
-parameters["mri_u1"  ] = 0.01
+parameters["mri_u1"  ] = -0.01
 parameters["mri_x"   ] = np.log(10)
 velocity_transport.run(5, parameters)
 