~ Changed sign on mri_tests 6 and 7. ~ Generate outline mesh on mri_run. ~ Updated voxel2 of interest on the real MRI data. ! Fixed bug where coloured voxels weren't consistently applied to real MRI data. ~ Made lots of changes to the mri spin plotting; some changes are a bit hacky, but do produce nice thesis pictures. + Added a nice S-vs-b plotter for the MRI data in Python. This is a glorious day: I no longer need to interact with MATLAB for producing graphs.

plotting/plot_mri_spins.py
@@ -57,7 +57,7 @@ def plot_quiver(simulation_no, filename_no_ext):
   if (filename_no_ext[-4:] == 'test'):
     norm = colors.Normalize(vmin=np.min(U), vmax=np.max(U))
   else:
-    norm = colors.Normalize(vmin=0, vmax=0.1)
+    norm = colors.Normalize(vmin=0, vmax=0.01)
   sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
   sm.set_array([])
 
@@ -82,9 +82,9 @@ def plot_quiver(simulation_no, filename_no_ext):
     ax.quiver(mat_vars['x_sample'][0][0][j*points_per_voxel_y:(j+1)*points_per_voxel_y, i*points_per_voxel_x:(i+1)*points_per_voxel_x], mat_vars['x_sample'][1][0][j*points_per_voxel_y:(j+1)*points_per_voxel_y, i*points_per_voxel_x:(i+1)*points_per_voxel_x], mat_vars['v_sample'][0][0][j*points_per_voxel_y:(j+1)*points_per_voxel_y, i*points_per_voxel_x:(i+1)*points_per_voxel_x], mat_vars['v_sample'][1][0][j*points_per_voxel_y:(j+1)*points_per_voxel_y, i*points_per_voxel_x:(i+1)*points_per_voxel_x], U, cmap=cmap, scale=scaling, scale_units='xy', angles='xy', width=0.005, headwidth=8, headaxislength=4, clip_on=False, norm=norm)
     ax.set_xticks([])
     ax.set_yticks([])
-    cbar = fig.colorbar(sm, cax=cax, orientation='horizontal', shrink=1.0, location='bottom', ticks=np.linspace(0, 0.1, 5))
+    cbar = fig.colorbar(sm, cax=cax, orientation='horizontal', shrink=1.0, location='bottom', ticks=np.linspace(0, 0.01, 5))
     cbar.set_label(r'$|\mathbf{u}|$ (m/s)', size=20)
-    cbar.set_ticks(np.linspace(0, 0.1, 5))
+    cbar.set_ticks(np.linspace(0, 0.01, 5))
 
   # Style.
   if (filename_no_ext == '2D_accelerating_test'):
@@ -94,30 +94,76 @@ def plot_quiver(simulation_no, filename_no_ext):
   elif (filename_no_ext == '2D_shear_test'):
     title = f"shear flow: $U_1={mat_vars['U_1'][0][0]:g}$, $U_2={mat_vars['U_2'][0][0]:g}$"
   else:
-    title = f"simulated flow in \quad \quad \quad"
+    title = f"simulated placenta flow in                       "
   ax.set_xlabel(r'$x$', fontsize=20)
   ax.set_ylabel(r'$y$', fontsize=20)
   fig.suptitle(r"Velocity field at each $\mathbf{x}_j$", fontsize=36, y=1.0)
   ax.set_title(f"{title}", fontsize=24, pad=30) # pad = 10
   if (filename_no_ext[-4:] != 'test'):
-    fig.text(0.53, 0.9205, f'{voxel_colour} voxel', fontsize=24, ha='left', color=f"tab:{voxel_colour}")
+    #fig.text(0.53, 0.9205, f'{voxel_colour} voxel', fontsize=24, ha='left', color=f"tab:{voxel_colour}")
+    fig.text(0.618, 0.9208, f'{voxel_colour} voxel', fontsize=24, ha='left', color=f"tab:{voxel_colour}")
   ax.tick_params(labelsize=20)
 
   # Output.
   fig.savefig(f"./images/mri-spins_quiver_{filename_no_ext}_{simulation_no}_{voxel_no}.png")
   plt.close(fig)
 
-def plot_s_vs_b(simulation_no, filename_no_ext):
+def plot_s_vs_b_mri_data():
+  # Load data.
+  import nibabel as nib
+  scan = nib.load('../../Dropbox/Documents/PhD/Miscellaneous/2022-06-27 George MRI GUI/PiP_Ox_059_WIP_DWI_19bvalues_6_1.nii').get_fdata()
+
+  # z-slice and voxels to inspect.
+  slice_n = 5
+  voxels = [
+    [190, 140, 'blue'],
+    [192, 139, 'green']
+  ]
+
+  import numpy as np
+  b = np.array([0, 1, 3, 9, 18, 32, 54, 88, 110, 147, 180, 200, 230, 270, 300, 350, 400, 450, 500])
+
+  title = r"real MRI data in                       "
+
+  for i in range(len(voxels)):
+    voxel_no = f"{voxels[i][0]},{voxels[i][1]}"
+    S = scan[voxels[i][1]-1, voxels[i][0]-1, slice_n-1, :]
+    S_x = None
+    S_y = None
+    plot_s_vs_b(i, 'real_mri', voxel_no, S, S_x, S_y, b, title, voxels[i][2], markersize=15, markeredgecolor='white')
+
+def plot_s_vs_b_simulated(simulation_no, filename_no_ext):
+  # Set voxel info.
   if (filename_no_ext[-4:] == 'test'):
     voxel_no = 1
+    voxel_colour = None
   else:
     voxel_no = 672
     voxel_colour = 'blue'
     # voxel_no = 491
     # voxel_colour = 'green'
 
+  # Import data.
   mat_vars = import_mat(simulation_no, filename_no_ext)
 
+  S   = mat_vars['S'][voxel_no-1][0]
+  S_x = mat_vars['S_x'][voxel_no-1][0]
+  S_y = mat_vars['S_y'][voxel_no-1][0]
+  b   = mat_vars['b'][0]
+
+  # titles.
+  if (filename_no_ext == '2D_accelerating_test'):
+    title = f"manufactured accelerating flow: $U={mat_vars['U_1'][0][0]:g}$, $X={mat_vars['X'][0][0]:.4f}$"
+  elif (filename_no_ext == '2D_rotational_test'):
+    title = f"manufactured rotational flow: $U_1={mat_vars['U_1'][0][0]:g}$, $U_2={mat_vars['U_2'][0][0]:g}$"
+  elif (filename_no_ext == '2D_shear_test'):
+    title = f"manufactured shear flow: $U_1={mat_vars['U_1'][0][0]:g}$, $U_2={mat_vars['U_2'][0][0]:g}$"
+  else:
+    title = f"simulated placenta flow in                       "
+  
+  plot_s_vs_b(simulation_no, filename_no_ext, voxel_no, S, S_x, S_y, b, title, voxel_colour, 0.618)
+
+def plot_s_vs_b(simulation_no, filename_no_ext, voxel_no, S, S_x, S_y, b, title, voxel_colour=None, x_padding=0.53, markersize=None, markeredgecolor=None):
   import matplotlib.patches as mpatches
   max_no_patches = 3
   handles = []
@@ -127,104 +173,102 @@ def plot_s_vs_b(simulation_no, filename_no_ext):
   import numpy as np
   import matplotlib.pyplot as plt
 
+  # Calculates axis limits.
+  b_range = b[-1] - b[0]
+  g_range = b[-1]**0.5 - b[0]**0.5
+  S_range = 1
+  eps = 0.02
+
+  # Figure setup.
   fig_s = plt.figure(figsize=(10, 10))
   ax_s = fig_s.add_subplot(111)
   ax_s.clear()
 
-  fig_sx = plt.figure(figsize=(10, 10))
-  ax_sx = fig_sx.add_subplot(111)
-  ax_sx.clear()
-
-  fig_sy = plt.figure(figsize=(10, 10))
-  ax_sy = fig_sy.add_subplot(111)
-  ax_sy.clear()
-
-  fig_sall = plt.figure(figsize=(10, 10))
-  ax_sall = fig_sall.add_subplot(111)
-  ax_sall.clear()
-
-  fig_gall = plt.figure(figsize=(10, 10))
-  ax_gall = fig_gall.add_subplot(111)
-  ax_gall.clear()
-
-  # Plot.
-  ax_sx  .plot(mat_vars['b'][0],      mat_vars['S_x'][voxel_no-1][0]/mat_vars['S_x'][voxel_no-1][0][0], 'o', c='tab:blue')
-  ax_sall.plot(mat_vars['b'][0],      mat_vars['S_x'][voxel_no-1][0]/mat_vars['S_x'][voxel_no-1][0][0], 'o', c='tab:blue', label=r'$S_x/S_{x,0}$')
-  ax_gall.plot(mat_vars['b'][0]**0.5, mat_vars['S_x'][voxel_no-1][0]/mat_vars['S_x'][voxel_no-1][0][0], 'o', c='tab:blue', label=r'$S_x/S_{x,0}$')
-  ax_sy  .plot(mat_vars['b'][0],      mat_vars['S_y'][voxel_no-1][0]/mat_vars['S_y'][voxel_no-1][0][0], 'o', c='tab:green')
-  ax_sall.plot(mat_vars['b'][0],      mat_vars['S_y'][voxel_no-1][0]/mat_vars['S_y'][voxel_no-1][0][0], 'o', c='tab:green', label=r'$S_y/S_{y,0}$')
-  ax_gall.plot(mat_vars['b'][0]**0.5, mat_vars['S_y'][voxel_no-1][0]/mat_vars['S_y'][voxel_no-1][0][0], 'o', c='tab:green', label=r'$S_y/S_{y,0}$')
-  ax_s   .plot(mat_vars['b'][0],      mat_vars['S'][voxel_no-1][0]  /mat_vars['S'][voxel_no-1][0][0],   'o', c='tab:orange')
-  ax_sall.plot(mat_vars['b'][0],      mat_vars['S'][voxel_no-1][0]  /mat_vars['S'][voxel_no-1][0][0],   'o', c='tab:orange', label=r'$S/S_0$')
-  ax_gall.plot(mat_vars['b'][0]**0.5, mat_vars['S'][voxel_no-1][0]  /mat_vars['S'][voxel_no-1][0][0],   'o', c='tab:orange', label=r'$S/S_0$')
-
-  # Style.
-  if (filename_no_ext == '2D_accelerating_test'):
-    title = f"accelerating flow: $U_1={mat_vars['U_1'][0][0]:g}$, $X={mat_vars['X'][0][0]:.4f}$"
-  elif (filename_no_ext == '2D_rotational_test'):
-    title = f"rotational flow: $U_1={mat_vars['U_1'][0][0]:g}$, $U_2={mat_vars['U_2'][0][0]:g}$"
-  elif (filename_no_ext == '2D_shear_test'):
-    title = f"shear flow: $U_1={mat_vars['U_1'][0][0]:g}$, $U_2={mat_vars['U_2'][0][0]:g}$"
-  else:
-    title = f"simulated flow in \quad \quad \quad"
-  ax_s   .set_xlabel(r'$b$', fontsize=20)
-  ax_sx  .set_xlabel(r'$b$', fontsize=20)
-  ax_sy  .set_xlabel(r'$b$', fontsize=20)
-  ax_sall.set_xlabel(r'$b$', fontsize=20)
-  ax_gall.set_xlabel(r'$g$', fontsize=20)
-  ax_s   .set_ylabel(r'$S/S_0$', fontsize=20)
-  ax_sx  .set_ylabel(r'$S_x/S_{x,0}$', fontsize=20)
-  ax_sy  .set_ylabel(r'$S_y/S_{y,0}$', fontsize=20)
-  ax_sall.set_ylabel(r'$S/S_0$', fontsize=20)
-  ax_sall.legend(handles=[handles[1], handles[0], handles[2]], labels=[r'$S/S_0$', r'$S_x/S_{x,0}$', r'$S_y/S_{y,0}$'], fontsize=20, loc='upper right')
-  ax_gall.legend(handles=[handles[1], handles[0], handles[2]], labels=[r'$S/S_0$', r'$S_x/S_{x,0}$', r'$S_y/S_{y,0}$'], fontsize=20, loc='upper right')
-  fig_s   .suptitle(r'$S/S_0$ vs $b$', fontsize=36, y=1.0)
-  fig_sx .suptitle(r'$S_x/S_{x,0}$ vs $b$', fontsize=36, y=1.0)
-  fig_sy   .suptitle(r'$S_y/S_{y,0}$ vs $b$', fontsize=36, y=1.0)
-  fig_sall.suptitle(r'$S/S_0$ vs $b$', fontsize=36, y=1.0)
-  fig_gall.suptitle(r'$S/S_0$ vs $g$', fontsize=36, y=1.0)
-  ax_s   .set_title(f'{title}', fontsize=24, pad=15)
-  ax_sx  .set_title(f'{title}', fontsize=24, pad=15)
-  ax_sy  .set_title(f'{title}', fontsize=24, pad=15)
-  ax_sall.set_title(f'{title}', fontsize=24, pad=15)
-  ax_gall.set_title(f'{title}', fontsize=24, pad=15)
+  # Plot S.
+  ax_s .plot(b,      S  /S[0],   'o', c='tab:orange', markersize=markersize, markeredgecolor=markeredgecolor)
+  ax_s .set_xlabel(r'$b$', fontsize=20)
+  ax_s .set_ylabel(r'$S/S_0$', fontsize=20)
+  fig_s.suptitle(r'$S/S_0$ vs $b$', fontsize=36, y=1.0)
+  ax_s .set_title(f'{title}', fontsize=24, pad=15)
+  ax_s .tick_params(labelsize=20)
   if (filename_no_ext[-4:] != 'test'):
-    fig_s   .text(0.53, 0.901, f'{voxel_colour} voxel', fontsize=24, ha='left', color=f"tab:{voxel_colour}")
-    fig_sx  .text(0.53, 0.901, f'{voxel_colour} voxel', fontsize=24, ha='left', color=f"tab:{voxel_colour}")
-    fig_sy  .text(0.53, 0.901, f'{voxel_colour} voxel', fontsize=24, ha='left', color=f"tab:{voxel_colour}")
-    fig_sall.text(0.53, 0.901, f'{voxel_colour} voxel', fontsize=24, ha='left', color=f"tab:{voxel_colour}")
-    fig_gall.text(0.53, 0.901, f'{voxel_colour} voxel', fontsize=24, ha='left', color=f"tab:{voxel_colour}")
-  ax_s   .tick_params(labelsize=20)
-  ax_sx  .tick_params(labelsize=20)
-  ax_sy  .tick_params(labelsize=20)
-  ax_sall.tick_params(labelsize=20)
-  ax_gall.tick_params(labelsize=20)
-  b_range = mat_vars['b'][0][-1] - mat_vars['b'][0][0]
-  g_range = mat_vars['b'][0][-1]**0.5 - mat_vars['b'][0][0]**0.5
-  S_range = 1
-  eps = 0.02
-  ax_s   .set_xlim([-eps*b_range, mat_vars['b'][0][-1] + eps*b_range])
-  ax_sx  .set_xlim([-eps*b_range, mat_vars['b'][0][-1] + eps*b_range])
-  ax_sy  .set_xlim([-eps*b_range, mat_vars['b'][0][-1] + eps*b_range])
-  ax_sall.set_xlim([-eps*b_range, mat_vars['b'][0][-1] + eps*b_range])
-  ax_gall.set_xlim([-eps*g_range, mat_vars['b'][0][-1]**0.5 + eps*g_range])
-  ax_s   .set_ylim ([-eps*S_range, 1 + eps*S_range])
-  ax_sx  .set_ylim([-eps*S_range, 1 + eps*S_range])
-  ax_sy  .set_ylim([-eps*S_range, 1 + eps*S_range])
-  ax_sall.set_ylim([-eps*S_range, 1 + eps*S_range])
-  ax_gall.set_ylim([-eps*S_range, 1 + eps*S_range])
-
-  fig_s  .savefig(f"./images/mri-spins_s-vs-b_{filename_no_ext}_{simulation_no}_{voxel_no}.png")
-  fig_sx .savefig(f"./images/mri-spins_sx-vs-b_{filename_no_ext}_{simulation_no}_{voxel_no}.png")
-  fig_sy .savefig(f"./images/mri-spins_sy-vs-b_{filename_no_ext}_{simulation_no}_{voxel_no}.png")
-  fig_sall.savefig(f"./images/mri-spins_sall-vs-b_{filename_no_ext}_{simulation_no}_{voxel_no}.png")
-  fig_gall.savefig(f"./images/mri-spins_gall-vs-b_{filename_no_ext}_{simulation_no}_{voxel_no}.png")
-
+    fig_s.text(x_padding, 0.9, f'{voxel_colour} voxel', fontsize=24, ha='left', color=f"tab:{voxel_colour}")
+  ax_s .set_xlim([-eps*b_range, b[-1] + eps*b_range])
+  ax_s .set_ylim ([-eps*S_range, 1 + eps*S_range])
+  fig_s.savefig(f"./images/mri-spins_s-vs-b_{filename_no_ext}_{simulation_no}_{voxel_no}.png")
   plt.close(fig_s)
-  plt.close(fig_sx)
-  plt.close(fig_sy)
-  plt.close(fig_sall)
-  plt.close(fig_gall)
+
+  # Plot others.
+  if (S_x is not None):
+    fig_sx = plt.figure(figsize=(10, 10))
+    ax_sx = fig_sx.add_subplot(111)
+    ax_sx.clear()
+
+    fig_sy = plt.figure(figsize=(10, 10))
+    ax_sy = fig_sy.add_subplot(111)
+    ax_sy.clear()
+
+    fig_sall = plt.figure(figsize=(10, 10))
+    ax_sall = fig_sall.add_subplot(111)
+    ax_sall.clear()
+
+    fig_gall = plt.figure(figsize=(10, 10))
+    ax_gall = fig_gall.add_subplot(111)
+    ax_gall.clear()
+
+    ax_sx  .plot(b,      S_x/S_x[0], 'o', c='tab:blue')
+    ax_sall.plot(b,      S_x/S_x[0], 'o', c='tab:blue', label=r'$S_x/S_{x,0}$')
+    ax_gall.plot(b**0.5, S_x/S_x[0], 'o', c='tab:blue', label=r'$S_x/S_{x,0}$')
+    ax_sy  .plot(b,      S_y/S_y[0], 'o', c='tab:green')
+    ax_sall.plot(b,      S_y/S_y[0], 'o', c='tab:green', label=r'$S_y/S_{y,0}$')
+    ax_gall.plot(b**0.5, S_y/S_y[0], 'o', c='tab:green', label=r'$S_y/S_{y,0}$')
+    ax_sall.plot(b,      S  /S[0],   'o', c='tab:orange', label=r'$S/S_0$')
+    ax_gall.plot(b**0.5, S  /S[0],   'o', c='tab:orange', label=r'$S/S_0$')
+
+    ax_sx  .set_xlabel(r'$b$', fontsize=20)
+    ax_sy  .set_xlabel(r'$b$', fontsize=20)
+    ax_sall.set_xlabel(r'$b$', fontsize=20)
+    ax_gall.set_xlabel(r'$g$', fontsize=20)
+    ax_sx  .set_ylabel(r'$S_x/S_{x,0}$', fontsize=20)
+    ax_sy  .set_ylabel(r'$S_y/S_{y,0}$', fontsize=20)
+    ax_sall.set_ylabel(r'$S/S_0$', fontsize=20)
+    ax_sall.legend(handles=[handles[1], handles[0], handles[2]], labels=[r'$S/S_0$', r'$S_x/S_{x,0}$', r'$S_y/S_{y,0}$'], fontsize=20, loc='upper right')
+    ax_gall.legend(handles=[handles[1], handles[0], handles[2]], labels=[r'$S/S_0$', r'$S_x/S_{x,0}$', r'$S_y/S_{y,0}$'], fontsize=20, loc='upper right')
+    fig_sx .suptitle(r'$S_x/S_{x,0}$ vs $b$', fontsize=36, y=1.0)
+    fig_sy   .suptitle(r'$S_y/S_{y,0}$ vs $b$', fontsize=36, y=1.0)
+    fig_sall.suptitle(r'$S/S_0$ vs $b$', fontsize=36, y=1.0)
+    fig_gall.suptitle(r'$S/S_0$ vs $g$', fontsize=36, y=1.0)
+    ax_sx  .set_title(f'{title}', fontsize=24, pad=15)
+    ax_sy  .set_title(f'{title}', fontsize=24, pad=15)
+    ax_sall.set_title(f'{title}', fontsize=24, pad=15)
+    ax_gall.set_title(f'{title}', fontsize=24, pad=15)
+    if (filename_no_ext[-4:] != 'test'):
+      fig_sx  .text(x_padding, 0.9, f'{voxel_colour} voxel', fontsize=24, ha='left', color=f"tab:{voxel_colour}")
+      fig_sy  .text(x_padding, 0.9, f'{voxel_colour} voxel', fontsize=24, ha='left', color=f"tab:{voxel_colour}")
+      fig_sall.text(x_padding, 0.9, f'{voxel_colour} voxel', fontsize=24, ha='left', color=f"tab:{voxel_colour}")
+      fig_gall.text(x_padding, 0.9, f'{voxel_colour} voxel', fontsize=24, ha='left', color=f"tab:{voxel_colour}")
+    ax_sx  .tick_params(labelsize=20)
+    ax_sy  .tick_params(labelsize=20)
+    ax_sall.tick_params(labelsize=20)
+    ax_gall.tick_params(labelsize=20)
+    ax_sx  .set_xlim([-eps*b_range, b[-1] + eps*b_range])
+    ax_sy  .set_xlim([-eps*b_range, b[-1] + eps*b_range])
+    ax_sall.set_xlim([-eps*b_range, b[-1] + eps*b_range])
+    ax_gall.set_xlim([-eps*g_range, b[-1]**0.5 + eps*g_range])
+    ax_sx  .set_ylim([-eps*S_range, 1 + eps*S_range])
+    ax_sy  .set_ylim([-eps*S_range, 1 + eps*S_range])
+    ax_sall.set_ylim([-eps*S_range, 1 + eps*S_range])
+    ax_gall.set_ylim([-eps*S_range, 1 + eps*S_range])
+
+    fig_sx .savefig(f"./images/mri-spins_sx-vs-b_{filename_no_ext}_{simulation_no}_{voxel_no}.png")
+    fig_sy .savefig(f"./images/mri-spins_sy-vs-b_{filename_no_ext}_{simulation_no}_{voxel_no}.png")
+    fig_sall.savefig(f"./images/mri-spins_sall-vs-b_{filename_no_ext}_{simulation_no}_{voxel_no}.png")
+    fig_gall.savefig(f"./images/mri-spins_gall-vs-b_{filename_no_ext}_{simulation_no}_{voxel_no}.png")
+
+    plt.close(fig_sx)
+    plt.close(fig_sy)
+    plt.close(fig_sall)
+    plt.close(fig_gall)
 
 def plot_spins(simulation_no, filename_no_ext):
   mat_vars = import_mat(simulation_no, filename_no_ext)